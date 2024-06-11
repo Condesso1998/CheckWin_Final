ad18f119b3388f8d7f9c5d4e1c0cf860c6ace428
Rui Condesso <ruicondesso98@gmail.com>
2024-06-11 11:02:23 +0100

    remoçao de erros / começo do inicio

--- app/src/main/java/pt/ipg/checkwin_final/MainActivity.kt
+++ app/src/main/java/pt/ipg/checkwin_final/MainActivity.kt
@@ -1,17 +1,15 @@
 package pt.ipg.checkwin_final
 
-
-
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -20,7 +18,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -39,16 +37,14 @@ import pt.ipg.checkwin_final.ui.theme.CheckWin_FinalTheme
 import java.text.NumberFormat
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContent {
             CheckWin_FinalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TipTimeLayout(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize()
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    TipTimeLayout()
                 }
             }
         }
@@ -56,145 +52,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun EditNumberField(
-    labelText: String,
-    value: String,
-    onValueChange: (String) -> Unit,
-    action: ImeAction = ImeAction.Next,
-    modifier: Modifier = Modifier
-) {
-    TextField(
-        value = value,
-        onValueChange = onValueChange,
-        label = { Text(text = labelText) },
-        singleLine = true,
-        keyboardOptions = KeyboardOptions.Default.copy(
-            keyboardType = KeyboardType.Number,
-            imeAction = action
-        ),
-        modifier = modifier
-
-    )
-
-}
-
-@Composable
-fun RoundUpTipRow(
-    checked: Boolean,
-    onCheckedChange: (Boolean) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-    ) {
-        Text(
-            text = stringResource(R.string.round_up_tip),
-            modifier = Modifier.weight(1.0F)
-        )
-        Switch(
-            checked = checked,
-            onCheckedChange = onCheckedChange
-        )
-    }
-}
-
-@Composable
-fun TipTimeLayout(modifier: Modifier = Modifier) {
-    var amountInput by remember {
-        mutableStateOf("")
-    }
-
-    var tipPercentageInput by remember {
-        mutableStateOf("")
-    }
-
-    var roundUpTip by remember {
-        mutableStateOf(false)
-    }
-
-    val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tipPercent = tipPercentageInput.toDoubleOrNull() ?: 0.0
-
-    val tip = calculateTip(amount, tipPercent, roundUpTip)
-
+fun TipTimeLayout() {
     Column(
-        modifier = modifier
+        modifier = Modifier
             .statusBarsPadding()
-            .padding(horizontal = 16.dp)
-            .verticalScroll(rememberScrollState())
+            .padding(horizontal = 40.dp)
             .safeDrawingPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = stringResource(R.string.num_vitorias),
+            text = stringResource(R.string.calculate_tip),
             modifier = Modifier
-                .padding(bottom = 16.dp)
+                .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-
-        EditNumberField(
-            labelText = stringResource(id = R.string.bill_amount),
-            value = amountInput,
-            onValueChange = { newValue -> amountInput = newValue },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        )
         Text(
-            text = stringResource(R.string.num_derrotas),
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-                .align(alignment = Alignment.Start)
-        )
-
-        EditNumberField(
-            labelText = stringResource(R.string.tip_percentage),
-            value = tipPercentageInput,
-            onValueChange = { newValue -> tipPercentageInput = newValue },
-            action = ImeAction.Done,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        )
-
-
-        RoundUpTipRow(
-            checked = roundUpTip,
-            onCheckedChange = { newValue -> roundUpTip = newValue},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        )
-
-        Text(
-            text = stringResource(R.string.tip_amount, tip),
+            text = stringResource(R.string.tip_amount, "$0.00"),
             style = MaterialTheme.typography.displaySmall
         )
+        Spacer(modifier = Modifier.height(150.dp))
     }
+}
+
+/**
+ * Calculates the tip based on the user input and format the tip amount
+ * according to the local currency.
+ * Example would be "$10.00".
+ */
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+    val tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun TipTimePreview() {
+fun TipTimeLayoutPreview() {
     CheckWin_FinalTheme {
-        TipTimeLayout(
-            modifier = Modifier.fillMaxSize()
-        )
+        TipTimeLayout()
     }
-}
-
-private fun calculateTip(
-    amount: Double,
-    tipPercent: Double = 15.0,
-    roundUpTip: Boolean = false
-): String {
-    var tip = (tipPercent / 100) * amount
-
-    if (roundUpTip) {
-        tip = kotlin.math.ceil(tip)
-    }
-
-    return NumberFormat.getCurrencyInstance().format(tip)
 }
