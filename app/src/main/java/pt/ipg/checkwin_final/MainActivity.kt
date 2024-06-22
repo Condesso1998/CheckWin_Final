78c5368fd300cce0fbca7033cf337eda4a954183
Rui Condesso <ruicondesso98@gmail.com>
2024-06-22 17:10:09 +0100

    os  2 botoes estao a funcionar

--- app/src/main/java/pt/ipg/checkwin_final/MainActivity.kt
+++ app/src/main/java/pt/ipg/checkwin_final/MainActivity.kt
@@ -9,11 +9,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -50,7 +46,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "Calcula Derrotas") {
+    NavHost(navController = navController, startDestination = "Inicio") {
+        composable("Inicio") {
+            Inicio(
+                navigateToDerrotas = { navController.navigate("Calcula Derrotas") },
+                navigateToVitorias = { navController.navigate("Calcula Vitorias") }
+            )
+        }
         composable("Calcula Derrotas") {
             CalculaDerrotas(
                 navigateToDerrotas = { navController.navigate("Calcula Vitorias") }
@@ -64,12 +66,8 @@ fun MyApp(modifier: Modifier = Modifier) {
     }
 }
 
-fun CalculaDerrotas(navigateToDerrotas: () -> Unit,modifier: Modifier = Modifier) {
-
-}
-
 @Composable
-fun CalculaVitorias(navigateToVitorias: () -> Unit, modifier: Modifier = Modifier) {
+fun Inicio(navigateToVitorias: () -> Unit, navigateToDerrotas: () -> Unit, modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -84,45 +82,35 @@ fun CalculaVitorias(navigateToVitorias: () -> Unit, modifier: Modifier = Modifie
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = stringResource(R.string.Bottoes),
+                text = "Escolha uma opção",
                 style = MaterialTheme.typography.displaySmall,
                 color = Color.White,
                 modifier = Modifier
                     .padding(bottom = 16.dp)
                     .align(alignment = Alignment.Start)
             )
-            Button(onClick = { navigateToVitorias() }) {
+            Button(onClick = navigateToDerrotas) {
+                Text(text = "Mudar para Derrotas")
+            }
+            Button(onClick = navigateToVitorias) {
                 Text(text = "Mudar para Vitorias")
             }
-
         }
     }
 }
 
 @Composable
-fun NewPage(navigateToVitorias: () -> Unit, modifier: Modifier = Modifier) {
-
-    var quantidadeJogosInput by remember {
-        mutableStateOf("")
-    }
-
-    var percentagemVitoriasageInput by remember {
-        mutableStateOf("0")
-    }
-
-    var roundUpTip by remember {
-        mutableStateOf(false)
-    }
+fun CalculaDerrotas(navigateToDerrotas: () -> Unit, modifier: Modifier = Modifier) {
+    var quantidadeJogosInput by remember { mutableStateOf("") }
+    var percentagemVitoriasageInput by remember { mutableStateOf("0") }
+    var roundUpTip by remember { mutableStateOf(false) }
 
     val quantidadeJogos = quantidadeJogosInput.toDoubleOrNull() ?: 0.0
     val percentagemVitorias = percentagemVitoriasageInput.toDoubleOrNull() ?: 0.0
 
     val derrotas = CalculaDerrotas(quantidadeJogos.toInt(), percentagemVitorias.toInt(), roundUpTip)
 
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-    ) {
+    Box(modifier = modifier.fillMaxSize()) {
         Image(
             painter = painterResource(id = R.drawable.fundo),
             contentDescription = null,
@@ -139,7 +127,7 @@ fun NewPage(navigateToVitorias: () -> Unit, modifier: Modifier = Modifier) {
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = stringResource(R.string.inserirJogos),
+                text = "Insira os Jogos",
                 color = Color.White,
                 modifier = Modifier
                     .padding(bottom = 16.dp)
@@ -147,7 +135,7 @@ fun NewPage(navigateToVitorias: () -> Unit, modifier: Modifier = Modifier) {
             )
 
             EditNumberFieldDerrotas(
-                labelText = stringResource(id = R.string.bill_quantidadeJogos),
+                labelText = "Quantidade de Jogos",
                 value = quantidadeJogosInput,
                 onValueChange = { newValue -> quantidadeJogosInput = newValue },
                 modifier = Modifier
@@ -155,7 +143,7 @@ fun NewPage(navigateToVitorias: () -> Unit, modifier: Modifier = Modifier) {
                     .padding(bottom = 16.dp)
             )
             Text(
-                text = stringResource(R.string.inserirVitorias),
+                text = "Insira a Percentagem de Vitórias",
                 color = Color.White,
                 modifier = Modifier
                     .padding(bottom = 16.dp)
@@ -163,7 +151,7 @@ fun NewPage(navigateToVitorias: () -> Unit, modifier: Modifier = Modifier) {
             )
 
             EditNumberFieldDerrotas(
-                labelText = stringResource(R.string.bill_quantidadeVitorias),
+                labelText = "Quantidade de Vitórias",
                 value = percentagemVitoriasageInput,
                 onValueChange = { newValue -> percentagemVitoriasageInput = newValue },
                 action = ImeAction.Done,
@@ -182,14 +170,94 @@ fun NewPage(navigateToVitorias: () -> Unit, modifier: Modifier = Modifier) {
             )
 
             Text(
-                text = stringResource(R.string.quantidadeDerrotas, derrotas),
+                text = "Quantidade de Derrotas: $derrotas",
                 style = MaterialTheme.typography.displaySmall
             )
-
-
-
+            Button(onClick = { navigateToDerrotas() }) {
+                Text(text = "Mudar para Vitorias")
+            }
         }
     }
-
-
 }
+
+@Composable
+fun NewPage(navigateToVitorias: () -> Unit, modifier: Modifier = Modifier) {
+    var quantidadeJogosInput by remember { mutableStateOf("") }
+    var percentagemVitoriasageInput by remember { mutableStateOf("0") }
+    var roundUpTip by remember { mutableStateOf(false) }
+
+    val quantidadeJogos = quantidadeJogosInput.toDoubleOrNull() ?: 0.0
+    val percentagemVitorias = percentagemVitoriasageInput.toDoubleOrNull() ?: 0.0
+
+    val derrotas = CalculaDerrotas(quantidadeJogos.toInt(), percentagemVitorias.toInt(), roundUpTip)
+
+    Box(modifier = modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.fundo),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = modifier
+                .statusBarsPadding()
+                .padding(horizontal = 16.dp)
+                .verticalScroll(rememberScrollState())
+                .safeDrawingPadding(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Insira os Jogos",
+                color = Color.White,
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .align(alignment = Alignment.Start)
+            )
+
+            EditNumberFieldDerrotas(
+                labelText = "Quantidade de Jogos",
+                value = quantidadeJogosInput,
+                onValueChange = { newValue -> quantidadeJogosInput = newValue },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+            Text(
+                text = "Insira a Percentagem de Vitórias",
+                color = Color.White,
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .align(alignment = Alignment.Start)
+            )
+
+            EditNumberFieldDerrotas(
+                labelText = "Quantidade de Vitórias",
+                value = percentagemVitoriasageInput,
+                onValueChange = { newValue -> percentagemVitoriasageInput = newValue },
+                action = ImeAction.Done,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            RoundUpTipRowDerrotas(
+                checked = roundUpTip,
+                onCheckedChange = { newValue -> roundUpTip = newValue },
+                navController = rememberNavController(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = "Quantidade de Derrotas: $derrotas",
+                style = MaterialTheme.typography.displaySmall
+            )
+            Button(onClick = { navigateToVitorias() }) {
+                Text(text = "Mudar para Derrotas")
+            }
+        }
+    }
+}
+
